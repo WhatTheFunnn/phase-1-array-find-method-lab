f3f4670850eda72b34a5f5ec4ea9d352c707aec0
Dan Souza <Souza.music@outlook.com>
2022-02-14 19:19:17 -0800

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin (record) {
+    const win = record.find(record=> record.result === "W") || "N/A";
+        console.log(win.year)
+    return win.year
+}
+
+
+
+
+
+    
+
